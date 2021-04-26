e0757076375b3ff09441d3de11ebb785d111a38f
DravenRedgrave <77494310+DravenRedgrave@users.noreply.github.com>
2021-04-26 10:50:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-const menuToggle = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('nav');
-  menuToggle.addEventListener('click', () => {
-      if (nav.className != 'active') {nav.className = 'active';}
-      else {nav.className = '';}
-  });
-
-
-
 $(document).ready(function(){
   $('.slider-big').slick({
     arrows:false,
@@ -74,3 +65,60 @@ $(document).ready(function(){
     ]
     });
     });
+
+    $(document).ready(function(){
+  $('.slider-content').slick({
+    arrows:false,
+    slidesToShow:3,
+    infinite:true,
+    autoplay:true,
+    pauseOnFocus:true,
+    adaptiveHeight:true,
+    pauseOnHover:true,
+    swipe:true,
+    TouchMove:true,
+    responsive:[
+      {
+        breakpoint: 768,
+        settings:{
+            arrows:false,
+          slidesToShow:2,
+        }
+      }, {
+        breakpoint: 922,
+        settings:{
+          arrows:false,
+          slidesToShow:3,
+        }
+      }, {
+        breakpoint: 480,
+        settings:{
+          arrows:false,
+          slidesToShow:1
+        }
+      }
+    ]
+  });
+});
+
+
+
+
+
+const menuToggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('nav');
+  menuToggle.addEventListener('click', () => {
+      if (nav.className != 'active') {nav.className = 'active';}
+      else {nav.className = '';}
+  });
+
+
+  $(document).ready(function() {
+    $('.spoler-title').click(function(event) {
+      if($('.spoiler-item').hasClass('one')){
+        $('.spoler-title').not($(this)).removeClass('active');
+        $('.spoiler-box').not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass('active').next().slideToggle(300);
+    })
+  })
